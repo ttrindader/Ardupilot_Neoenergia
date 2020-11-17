10124e5d662aa580421978bd033df6f6c82f2984
Mathaus Ferreira <mathausfsilva@hotmail.com>
2020-11-17 15:47:14 -0300

    Arrumei erro no auto com o get_gaidn()

--- Aero4River/mode_auto.cpp
+++ Aero4River/mode_auto.cpp
@@ -768,7 +768,7 @@ void ModeAuto::wp_run()
     // call attitude controller
     if (auto_yaw.mode() == AUTO_YAW_HOLD) {
         // roll & pitch from waypoint controller, yaw rate from pilot
-        attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(wp_nav->get_roll()*get_gadin(), wp_nav->get_pitch()*get_gain(), target_yaw_rate*get_gain());
+        attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(wp_nav->get_roll()*get_gain(), wp_nav->get_pitch()*get_gain(), target_yaw_rate*get_gain());
     } else {
         // roll, pitch from waypoint controller, yaw heading from auto_heading()
         attitude_control->input_euler_angle_roll_pitch_yaw(wp_nav->get_roll()*get_gain(), wp_nav->get_pitch()*get_gain(), auto_yaw.yaw()*get_gain(), true);
