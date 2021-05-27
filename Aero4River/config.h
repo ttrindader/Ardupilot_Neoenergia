ea064447bc8a8c269d2c7ac50703d9ac4db9b691
Mathaus Ferreira <mathausfsilva@hotmail.com>
2021-05-27 19:21:37 -0300

    reverting acro gains

--- Aero4River/config.h
+++ Aero4River/config.h
@@ -473,11 +473,11 @@
 
 // Acro Mode
 #ifndef ACRO_RP_P
- # define ACRO_RP_P                 4.5f
+ # define ACRO_RP_P                 1.0f
 #endif
 
 #ifndef ACRO_YAW_P
- # define ACRO_YAW_P                4.5f
+ # define ACRO_YAW_P                1.0f
 #endif
 
 #ifndef ACRO_LEVEL_MAX_ANGLE
