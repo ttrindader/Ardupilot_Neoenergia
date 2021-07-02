abb41a63d361a8e497dcd74f299416edda197f7b
Mathaus Ferreira <mathausfsilva@hotmail.com>
2021-07-02 09:44:11 -0300

    Only Stabilize working well ( i think so)

--- Aero4River/mode_stabilize.cpp
+++ Aero4River/mode_stabilize.cpp
@@ -63,10 +63,10 @@ void ModeStabilize::run(){
     }
 
     // call attitude controller
-    attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(target_roll*get_gain(), target_pitch*get_gain(), target_yaw_rate*get_gain());
-    // get_pilot_desired_forces(target_roll,target_pitch,target_yaw_rate);
+    // attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(target_roll*get_gain(), target_pitch*get_gain(), target_yaw_rate*get_gain());
+    get_pilot_desired_forces(target_roll,target_pitch,target_yaw_rate);
 
-    // attitude_control->input_rate_stabilize_roll_pitch_yaw(target_roll*get_gain(),target_pitch*get_gain(),target_yaw_rate*get_gain());
+    attitude_control->input_rate_stabilize_roll_pitch_yaw(target_roll*get_gain(),target_pitch*get_gain(),target_yaw_rate*get_gain());
 
     // output pilot's throttle
     attitude_control->set_throttle_out(get_pilot_desired_throttle(),false,g.throttle_filt);
